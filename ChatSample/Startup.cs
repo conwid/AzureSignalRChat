9e0a558568105cbbd899a32405176c6f2e6c0c38
conwid <akos@dotnetfalcon.com>
2019-06-10 23:50:53 +0200

    Add files via upload

---
+++ ChatSample/Startup.cs
@@ -0,0 +1,35 @@
+﻿using ChatSample.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ChatSample
+{
+    public class Startup
+    {
+        private static string connectionString = "";
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR().AddAzureSignalR(connectionString);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseFileServer();
+
+            app.UseAzureSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/chat");
+            });
+        }
+    }
+}
